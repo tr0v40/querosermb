b444ce7ee98ff125908ea698a71dae2a0b5e3122
Ricardo Fernandes <ricardofernandesdev@gmail.com>
2021-04-02 11:58:28 -0300

    model tests

---
+++ querosermb/mms_api/tests/test_models.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.test import TestCase
+
+from mms_api.models import CriptoValores
+
+class TestMmsModel(TestCase):
+    def setUp(self):
+        self.cripto = CriptoValores.objects.create(
+            pair = 1,
+            mms_timestamp = 1617374756,
+            close = 1.1,
+        )
+    
+    def test_choice_btc_true(self):
+        btc = CriptoValores.objects.get(mms_timestamp=self.cripto.mms_timestamp)
+        self.assertEqual(btc.get_pair_display(), 'BRLBTC')
+    
+    def test_choice_btc_false(self):
+        btc = CriptoValores.objects.get(mms_timestamp=self.cripto.mms_timestamp)
+        self.assertNotEqual(btc.get_pair_display(), 'BRLETH')
+
+    def test_return_true(self):
+        btc = CriptoValores.objects.get(mms_timestamp=self.cripto.mms_timestamp)
+        self.assertEqual(btc.__str__(), '1617374756')
+    
+    def test_return_False(self):
+        btc = CriptoValores.objects.get(mms_timestamp=self.cripto.mms_timestamp)
+        self.assertNotEqual(btc.__str__(), '222222')
+
