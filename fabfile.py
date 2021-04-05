d47e34f2f9bd8294c90fad7d813fdcee14a2a487
Ricardo Fernandes <ricardofernandesdev@gmail.com>
2021-04-04 22:50:16 -0300

    fabric deploy

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
- 
+# import os, re
+# import glob
+# import StringIO
+
+from fabric.api import task, run, cd, env, prefix, sudo, settings
 from fabric.api import *
-from fabric.operations import local
+# from fabric.operations import local
 
  
-env.hosts = ['178.128.152.77']
+env.hosts = ['198.211.114.165']
 env.user  = 'root'
 
 @task
 def full_instalattion():
-#     remote_info()
-#     install_ubuntu_dependencies()
-#     git_clone()
-#     psql_conf()
-#     pg_create_user()
-#     pg_create_database()
-#     permissions()
-#     virtualenv_install()
-    allow_ports()
+    # remote_info()
+    # install_ubuntu_dependencies()
+    # git_clone()
+    # psql_conf()
+    # pg_create_user()
+    # pg_create_database()
+    # permissions()
+    # virtualenv_install()
+    populate()
+    gunicorn_config()
+    nginx_conf()
+    restart_all()
 
 def restart(command):
     return sudo('systemctl restart %s' % command)
@@ -38,32 +45,32 @@ def git_clone():
 def git_pull():
     sudo('cd /home/ && git pull')
 
+def teste():
+    sudo('psql ')
 def _run_as_pg(command):
-    return sudo('sudo -u postgres %s' % command)
+    return run('sudo -u postgres %s' % command)
 
-def pg_create_user(username='querosermb', password='password'):
-    _run_as_pg('''psql -t -A -c "CREATE USER %(username)s WITH PASSWORD %(password)s;"''' % locals())
+def pg_create_user():
+    with settings(warn_only=True):
+        run("sudo -su postgres createuser querosermb")
 
-def pg_create_database(database='querosermb_db', owner='querosermb'):
-    _run_as_pg('createdb %(database)s -O %(owner)s' % locals())
+def pg_create_database():
+    run('sudo -u postgres createdb querosermb_db --owner=querosermb')
 
 def permissions():
     _run_as_pg('''psql -t -A -c "GRANT ALL PRIVILEGES ON DATABASE querosermb_db TO querosermb"''')
-#     _run_as_pg("GRANT ALL ON ALL TABLES IN SCHEMA public to querosermb;")
-#     _run_as_pg("GRANT ALL ON ALL SEQUENCES IN SCHEMA public to querosermb;")
-#     _run_as_pg("GRANT ALL ON ALL FUNCTIONS IN SCHEMA public to querosermb;")
+
+def venv():
+    env = ('source /home/querosermb/querosermb/_virtualenv/bin/activate && ')
+    return env
 
 def virtualenv_install():
     sudo('virtualenv /home/querosermb/querosermb/_virtualenv')
     sudo(venv() +  'pip3 install -r /home/querosermb/requirements.txt')
-    run(venv() +  'python manage.py migrate')
+    run(venv() +  'cd /home/querosermb/querosermb/ && python manage.py migrate')
 
-def venv():
-    env = ('source /home/querosermb/querosermb/_virtualenv/bin/activate &&')
-    return env
-
-def allow_ports():
-    sudo(venv() + 'ufw allow 8000')
+def populate():
+    run(venv() +  'cd /home/querosermb/querosermb/ && python populate.py')
 
 def psql_conf():
     sudo('systemctl stop postgresql')
@@ -71,5 +78,20 @@ def psql_conf():
     sudo('systemctl start postgresql')
 
 def gunicorn_config():
+    sudo('cp /home/querosermb/meta/gunicorn.service /etc/systemd/system/')
     sudo('systemctl start gunicorn')
     sudo('systemctl enable gunicorn')
+    sudo('systemctl daemon-reload')
+    sudo('systemctl restart gunicorn')
+
+def nginx_conf():
+    sudo('cp /home/querosermb/meta/querosermb.conf /etc/nginx/sites-available/querosermb')
+    sudo('ln -s /etc/nginx/sites-available/querosermb /etc/nginx/sites-enabled')
+    sudo('systemctl restart nginx')
+    sudo("ufw allow 'Nginx Full'")
+
+def restart_all():
+    sudo('systemctl daemon-reload')
+    sudo('systemctl restart gunicorn')
+    sudo('nginx -t')
+    sudo('systemctl restart nginx')
