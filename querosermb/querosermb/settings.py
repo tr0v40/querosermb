19ac5ee717fb2177f46e3e5fe5bc9348033ea777
Ricardo Fernandes <ricardofernandesdev@gmail.com>
2021-04-04 21:20:39 -0300

    password

--- querosermb/querosermb/settings.py
+++ querosermb/querosermb/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'querosermb_db',
         'USER': 'querosermb',
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': ''
     }
